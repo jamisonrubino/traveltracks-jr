7b052ed12a428f87be6e748644a736ad6ee74e3d
Jamison Rubino <jamison.rubino@gmail.com>
2017-09-11 21:21:48 +0000

    Fixed some PostgreSQL issues.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,12 +2,8 @@ class UsersController < ApplicationController
   def spotify
     config = {
       :access_token => request.env['omniauth.auth'].credentials.token,
-      :raise_errors => true,  # choose between returning false or raising a proper exception when API calls fails
-      # Connection properties
-      :retries       => 0,    # automatically retry a certain number of times before returning
-      :read_timeout  => 10,   # set longer read_timeout, default is 10 seconds
-      :write_timeout => 10,   # set longer write_timeout, default is 10 seconds
-      :persistent    => false # when true, make multiple requests calls using a single persistent connection. Use +close_connection+ method on the client to manually clean up sockets
+      :raise_errors => true,
+      :retries       => 0
     }
     puts request.env['omniauth.auth'].credentials
     spotify_user = Spotify::User.new(config)
--- db/migrate/20170909170829_create_songs.rb
+++ db/migrate/20170909170829_create_songs.rb
@@ -1,4 +1,4 @@
-class CreateSong < ActiveRecord::Migration[5.1]
+class CreateSongs < ActiveRecord::Migration[5.1]
   def change
     create_table :songs do |t|
       t.string :title
