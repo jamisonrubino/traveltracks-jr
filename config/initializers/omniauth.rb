fc334067b19c3bdc36d8b143a75bc887fd93483d
Jamison Rubino <jamison.rubino@gmail.com>
2017-09-11 19:21:17 +0000

    Fixing ENV variable issues still...

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,6 @@
+
+puts ENV["CLIENT_ID"]
+
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :spotify, ENV["CLIENT_ID"], ENV["CLIENT_SECRET"], scope: 'playlist-modify-private user-library-read user-read-birthdate user-read-email playlist-read-private'
 end
