b5664199dcabd9c29b07d9a48f87493eeabd03b9
Jamison Rubino <jamison.rubino@gmail.com>
2017-09-11 19:08:38 +0000

    Fixing env variables not registering in omniauth.rb

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :spotify, ENV["CLIENT_ID"], ENV["CLIENT_SECRET"], scope: 'playlist-modify-private user-library-read user-read-birthdate user-read-email playlist-read-private'
+  provider :spotify, "2557307048c8434b91848460d38cffcd", "dea0e75fbc2f414a82fd40ff8b9122c9", scope: 'playlist-modify-private user-library-read user-read-birthdate user-read-email playlist-read-private'
 end
 
 # to pull from user's top artists and tracks, include scope:
