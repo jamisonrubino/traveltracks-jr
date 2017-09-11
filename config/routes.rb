6f7bd2f0fbfdae94dd2d78e3acbe5e5aa9d11c45
Jamison Rubino <jamison.rubino@gmail.com>
2017-09-11 21:55:36 +0000

    Changing Spotify::User to Spotify::Client

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :songs
   resources :playlists, only: [:show, :create]
   resources :clients
-  get '/auth/spotify/callback', to: 'users#spotify'
+  get '/auth/spotify/callback', to: 'clients#spotify'
   root "welcome#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
