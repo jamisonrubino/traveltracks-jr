79f85277c17ed0a368854563524f67641e36e7de
Jamison Rubino <jamison.rubino@gmail.com>
2017-09-14 18:42:03 +0000

    Commenting out before_filter

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,9 @@
 class UsersController < ApplicationController
-  # skip_before_filter :require_login
+  skip_before_filter :require_login
   
   def spotify
     session[:spotify_key] = request.env['omniauth.auth'].credentials.token
-    session[:spotify_id] = request.env['omniauth.auth'].extra['raw-info'].id
+    # session[:spotify_id] = request.env['omniauth.auth'].extra['raw_info'].id
     redirect_to root_path
   end
 
