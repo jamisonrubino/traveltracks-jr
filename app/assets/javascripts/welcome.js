d1c5369af668294ed43cffc23358858d30694cf7
Jamison Rubino <jamison.rubino@gmail.com>
2017-09-15 21:47:11 +0000

    Playing with time_input filter.

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,10 +1,10 @@
-var inputField = $("#time_input");
-
-inputField.change(function() {
+function validateHhMm(inputField) {
     var isValid = /^([0-1]?[0-9]|2[0-4]):([0-5][0-9])(:[0-5][0-9])?$/.test(inputField.val());
     if (isValid) {
         inputField.css("background", "#bfa");
     } else {
         inputField.css("background", "#fba");
     }
-});
+}
+
+$("#time_input").change(validateHhMm($("#time_input")));
