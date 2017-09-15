e5c77bfed656bc3fca22015d97e5a9cd6876fa3c
Jamison Rubino <jamison.rubino@gmail.com>
2017-09-15 04:43:20 +0000

    Playing with text field.

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,7 +1,3 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://coffeescript.org/
-
 function validateHhMm(inputField) {
     var isValid = /^([0-1]?[0-9]|2[0-4]):([0-5][0-9])(:[0-5][0-9])?$/.test(inputField.value);
     if (isValid) {
