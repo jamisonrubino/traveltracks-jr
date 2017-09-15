c51d049f6284511840532e4815e8a913c5507390
Jamison Rubino <jamison.rubino@gmail.com>
2017-09-15 21:21:14 +0000

    Changed javascript_include_tag

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,4 +1,5 @@
-function validateHhMm(inputField) {
+function validateHhMm() {
+    inputField = $("#time_input");
     var isValid = /^([0-1]?[0-9]|2[0-4]):([0-5][0-9])(:[0-5][0-9])?$/.test(inputField.val());
     if (isValid) {
         inputField.css("background", "#bfa");
@@ -9,4 +10,4 @@ function validateHhMm(inputField) {
     return isValid;
 }
 
-$("#time_input").change(validateHhMm(this));
+$("#time_input").change(validateHhMm());
