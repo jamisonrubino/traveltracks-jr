9573e5970097dd212ce46a5c071fb27f4bd06729
Jamison Rubino <jamison.rubino@gmail.com>
2017-09-15 04:06:42 +0000

    Playing with text field.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,14 @@
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
+
+function validateHhMm(inputField) {
+    var isValid = /^([0-1]?[0-9]|2[0-4]):([0-5][0-9])(:[0-5][0-9])?$/.test(inputField.value);
+    if (isValid) {
+        inputField.style.backgroundColor = '#bfa';
+    } else {
+        inputField.style.backgroundColor = '#fba';
+    }
+
+    return isValid;
+}
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,4 @@
 module ApplicationHelper
-    # helper_method :
 
     def current_user
         Spotify::Client.new(access_token: session[:spotify_key]) if session[:spotify_key]
