cd1015982287f1881995001c30772e3c0e38c455
Jamison Rubino <jamison.rubino@gmail.com>
2017-09-15 02:05:12 +0000

    Fixing redirect loop.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,7 @@ class ApplicationController < ActionController::Base
   private
   
   def require_login
-      redirect_to root_path unless current_user
+    puts request.path
+    redirect_to root_path unless current_user || request.path == "/"
   end
 end
