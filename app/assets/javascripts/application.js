881fa268f8baaad5b1f7511ae200e61197bc494c
Jamison Rubino <jamison.rubino@gmail.com>
2017-09-15 04:47:35 +0000

    Playing with time input.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,18 +12,6 @@
 //
 //= require rails-ujs
 //= require turbolinks
+//= require jquery
 //= require_tree .
 
-function validateHhMm(inputField) {
-    var isValid = /^([0-1]?[0-9]|2[0-4]):([0-5][0-9])(:[0-5][0-9])?$/.test(inputField.value);
-    if (isValid) {
-        inputField.style.backgroundColor = '#bfa';
-    } else {
-        inputField.style.backgroundColor = '#fba';
-    }
-
-    return isValid;
-}
-
-var timeInput = document.getElementById("time_input");
-timeInput.change(validateHhMm(this));
