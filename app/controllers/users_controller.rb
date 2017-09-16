4cead7b4bff1cbd0888b958be34e30e49458c4f8
Jamison Rubino <jamison.rubino@gmail.com>
2017-09-16 22:37:35 +0000

    Added puts authtoken

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
   
   def spotify
     session[:spotify_key] = request.env['omniauth.auth'].credentials.token
+    puts request.env['omniauth.auth'].credentials.token
     # session[:spotify_id] = request.env['omniauth.auth'].extra['raw_info'].id
     redirect_to root_path
   end
