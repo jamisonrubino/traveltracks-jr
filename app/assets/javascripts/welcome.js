26d976ff97d33d6cdc0c75a6e5ae5eb31d0268a4
Jamison Rubino <jamison.rubino@gmail.com>
2017-09-17 23:59:51 +0000

    Added Maps directions inputs.

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,10 +1,15 @@
-function validateHhMm(inputField) {
-    var isValid = /^([0-1]?[0-9]|2[0-4]):([0-5][0-9])(:[0-5][0-9])?$/.test(inputField.val());
-    if (isValid) {
-        inputField.css("background", "#bfa");
-    } else {
-        inputField.css("background", "#fba");
-    }
-}
-var timeInput = $("#time_input");
-$(document).on("change", timeInput, validateHhMm(timeInput));
+// function validateHhMm(inputField) {
+//     var isValid = /^([0-1]?[0-9]|2[0-4]):([0-5][0-9])(:[0-5][0-9])?$/.test(inputField.val());
+//     if (isValid) {
+//         inputField.css("background", "#bfa");
+//     } else {
+//         inputField.css("background", "#fba");
+//     }
+// }
+// var timeInput = $("#time_input");
+// $(document).on("change", timeInput, validateHhMm(timeInput));
+
+$("#maps-directions-link a").click(function() {
+    $('#maps-directions-input').removeClass('hide'); 
+    $('#maps-directions-link').addClass('hide');
+});
