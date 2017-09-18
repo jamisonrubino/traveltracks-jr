982be2035443685c04302be060d978e542b93b47
Jamison Rubino <jamison.rubino@gmail.com>
2017-09-18 03:51:59 +0000

    Adding genre options.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,6 @@ class UsersController < ApplicationController
   
   def spotify
     session[:spotify_user] = RSpotify::User.new(request.env['omniauth.auth'])
-    session[:auth_token] = requent.env['omniauth.auth'].credentials.token
-    # session[:spotify_id] = request.env['omniauth.auth'].extra['raw_info'].id
     redirect_to root_path
   end
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,3 @@
 module ApplicationHelper
 
-    def current_user
-        Spotify::Client.new(access_token: session[:spotify_key]) if session[:spotify_key]
-    end
 end
