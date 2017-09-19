3d28b75f733a42a2ce9512772fbbaa0ab0cdca9d
Jamison Rubino <jamison.rubino@gmail.com>
2017-09-19 03:20:59 +0000

    Working on track adding algorithm.

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -59,7 +59,7 @@ class PlaylistsController < ApplicationController
         end
       end
       playlist_pool = RSpotify::Recommendations.generate(seed_genres: genres, limit:100) #
-    elseif params[:pool] == "my_top_tracks"
+    elsif params[:pool] == "my_top_tracks"
       playlist_pool = session[:spotify_user].saved_tracks(limit:50)
       playlist_pool += session[:spotify_user].saved_tracks(limit:50, offset:50)
     end
