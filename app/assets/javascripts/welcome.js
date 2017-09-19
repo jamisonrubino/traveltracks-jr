12dd3a6d43044803eb8747c17e543e702adbf836
Jamison Rubino <jamison.rubino@gmail.com>
2017-09-19 01:51:50 +0000

    Allowing multiple genre seeds.

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,26 +1,6 @@
-// function validateHhMm(inputField) {
-//     var isValid = /^([0-1]?[0-9]|2[0-4]):([0-5][0-9])(:[0-5][0-9])?$/.test(inputField.val());
-//     if (isValid) {
-//         inputField.css("background", "#bfa");
-//     } else {
-//         inputField.css("background", "#fba");
-//     }
-// }
-// var timeInput = $("#time_input");
-// $(document).on("change", timeInput, validateHhMm(timeInput));
-
 $(document).ready(function() {
     $("input#pool_genre").change(addHideGenres);
     $("input#pool_top_tracks").change(addHideGenres);
 });
-
-// function addHideGenres() {
-//     if ($("input:radio[name=pool]#pool_genre").is(":checked")) {
-//         $("#genre-seeds").removeClass("hide");
-//     }
-//     if ($("input:radio[name=pool]#pool_top_tracks").is(":checked")) {
-//         $("#genre-seeds").addClass("hide");
-//     }
-// }
 
 var genreNum = "two";
