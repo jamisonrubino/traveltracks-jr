da129d3649624a318d4944a6217f4b36f513985a
Jamison Rubino <jamison.rubino@gmail.com>
2017-09-19 02:19:33 +0000

    Multiple genre seeds.

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -8,8 +8,7 @@ var genreNum = "two";
 function addHideGenres() {
     if ($("input:radio[name=pool]#pool_genre").is(":checked")) {
         $("#genre-seeds").removeClass("hide");
-    } else if (($("input:radio[name=pool]#pool_top_tracks").is(":checked")) {
+    } else if ($("input:radio[name=pool]#pool_top_tracks").is(":checked")) {
         $("#genre-seeds").addClass("hide");
     }
-    
 }
