6d79c4b8c756ebaa6931b95c145160b83e9e9381
Jamison Rubino <jamison.rubino@gmail.com>
2017-09-21 22:28:02 +0000

    Working on playlist track adding algorithm.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 require 'rspotify/oauth'
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :spotify, ENV["CLIENT_ID"], ENV["CLIENT_SECRET"], scope: 'playlist-modify-private user-library-read user-read-birthdate user-read-email playlist-read-private playlist-read-collaborative user-top-read user-read-private'
+  provider :spotify, ENV["CLIENT_ID"], ENV["CLIENT_SECRET"], scope: 'playlist-modify-private playlist-modify-public user-library-read user-read-birthdate user-read-email playlist-read-private playlist-read-collaborative user-top-read user-read-private'
 end
