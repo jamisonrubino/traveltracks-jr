35c56ad9d9681d6ccdb7f3f9e6d5238efc5802d9
Jamison Rubino <jamison.rubino@gmail.com>
2017-09-22 05:25:23 +0000

    Updating styling.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,16 +9,7 @@ class UsersController < ApplicationController
     puts session['spotify_user_id']
     puts session['spotify_user']['email']
     
-    # session[:saved_tracks_1] = session['spotify_user'].saved_tracks(limit: 50, offset: 0)
-    
-    # session[:saved_tracks_2] = session['spotify_user'].saved_tracks(limit: 50, offset: 50)
-    
-    # session[:saved_tracks_3] = session['spotify_user'].saved_tracks(limit: 50, offset: 100)
-    
-    puts session[:saved_tracks_1]
-    # puts session[:saved_tracks_2]
-    # puts session[:saved_tracks_3]
-    
+
     redirect_to root_path
   end
   
