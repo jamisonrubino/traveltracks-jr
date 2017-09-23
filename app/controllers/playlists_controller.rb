7aafeb97a5aef433755843f2fd84b1a4b6b6b946
Jamison Rubino <jamison.rubino@gmail.com>
2017-09-23 03:06:08 +0000

    Refactored playlist#create into functions. Will work on more later.

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -27,125 +27,150 @@ class PlaylistsController < ApplicationController
   # POST /playlists.json
   def create
     # if user specifies starting point and destination
-    spotify_user = RSpotify::User.new(session['spotify_user'])
     
-    if params[:directions][:start].size > 0 && params[:directions][:destination].size > 0
-      start = params[:directions][:start]
-      destination = params[:directions][:destination]
-      directions = GoogleDirections.new(start, destination)
-      unless directions.status == ("NOT_FOUND" || "OVER_QUERY_LIMIT")
-        playlist_time = directions.drive_time_in_minutes
-      end
-      
-    # if user manually enters time
-    elsif params[:time][:hours].size > 0 || params[:time][:minutes].size > 0
-      hours = params[:time][:hours].to_i ||= 0
-      minutes = params[:time][:minutes].to_i ||= 0
-      playlist_time = hours * 60 + minutes
-    else
-      playlist_time = 0
-      flash[:notice] = "There was an error with your playlist time. Double-check your input before resubmitting."
-      redirect_to root_path
-    end
-    
-    # if trip is longer than six hours
-    
-    if playlist_time > 360
-      playlist_time = 360
-    end
-    
-    # SETTING POOL OF TRACKS FOR NEW PLAYLIST
-    if params[:pool] == "genre"
-      genres = []
-      genre_options = [params[:genre_seed_one], params[:genre_seed_two], params[:genre_seed_three]]
-      genre_options.each do |opt|
-        unless opt == ""
-          genres << opt
+    def set_time
+      if params[:directions][:start].size > 0 && params[:directions][:destination].size > 0
+        start = params[:directions][:start]
+        destination = params[:directions][:destination]
+        directions = GoogleDirections.new(start, destination)
+        unless directions.status == ("NOT_FOUND" || "OVER_QUERY_LIMIT")
+          playlist_time = directions.drive_time_in_minutes
         end
-      end
-      if genres.size == 1
-        genres = genres[0].to_s
-      end
-      recs = RSpotify::Recommendations.generate(seed_genres: genres, limit: 100)
-      playlist_pool = []
-      recs.tracks.each do |track|
-        playlist_pool << track
-      end
-      puts playlist_pool
-    elsif params[:pool] == "top_tracks"
-      puts "my_top_tracks if branch"
-      playlist_pool_1 = spotify_user.saved_tracks(limit: 50, offset: 0)
-      playlist_pool_2 = spotify_user.saved_tracks(limit: 50, offset: 50)
-      playlist_pool_3 = spotify_user.saved_tracks(limit: 50, offset: 100)
-      playlist_pool_4 = spotify_user.saved_tracks(limit: 50, offset: 150)
-      
-      playlist_pool = []
-      playlist_pool += playlist_pool_1 + playlist_pool_2 + playlist_pool_3 + playlist_pool_4
-    end
-    
-    # TO-DO: CREATE POOL LENGTH VARIABLE, CHECK AGAINST TRIP LENGTH; SUGGEST SAVING TRACKS OR ADDING GENRE SEEDS IF POOL LENGTH IS SHORTER
-    # RANDOMLY ADD TO PT (PLAYLIST TRACKS) ARRAY UNLESS IT WOULD EXCEED PLAYLIST_TIME
-    pt = []
-    ps = playlist_pool.size
-    puts "playlist pool size: #{ps}"
-    ptime = 0
-    until (playlist_time - ptime).abs.round(2) <= 2.50
-      if playlist_pool.size > 1
-        rn = Random.rand(playlist_pool.size-1)
-        unless ptime + playlist_pool[rn].duration_ms/60000 > playlist_time+2.50
-          pt << playlist_pool[rn]
-          playlist_pool.delete_at(rn)
-          ptime += playlist_pool[rn].duration_ms/60000
-          puts "ptime: #{ptime}"
-        end
+        
+      # if user manually enters time
+      elsif params[:time][:hours].size > 0 || params[:time][:minutes].size > 0
+        hours = params[:time][:hours].to_i ||= 0
+        minutes = params[:time][:minutes].to_i ||= 0
+        playlist_time = hours * 60 + minutes
       else
-        flash[:notice] = "Failure building playlist pool. Try saving more tracks or selecting different genres."
-        break
+        playlist_time = 0
+        flash[:notice] = "There was an error with your playlist time. Double-check your input before resubmitting."
         redirect_to root_path
       end
+    
+      # if trip is longer than six hours
+      if playlist_time > 360
+        playlist_time = 360
+      end
+      
+      playlist_time
     end
     
-    puts "playlist_time: #{playlist_time}"
-    puts "ptime: #{ptime}"
-    puts pt.size
-    
-    if playlist_time - ptime > 6
-      flash[:notice] = "Your playlist pool was shorter than your trip time. Try using genre seeds or saving more Spotify tracks to your library."
-    end
-
-
-    # CREATING NEW PLAYLIST
-    playlist_name = "My Roadtrip Playlist"
-    playlist_name += " #{params[:directions][:start] if params[:directions][:start]} to #{params[:directions][:destination] if params[:directions][:destination]}" if params[:pool] = params[:directions][:start].size > 0 && params[:directions][:destination].size > 0
-    
-    puts "Creating playlist!"
-    playlist = spotify_user.create_playlist!(playlist_name)
     
     
-    puts "Adding tracks!"
-    # ADDING SELECTED TRACKS TO NEW PLAYLIST IN BLOCKS OF 10 (A[I][10TRACKS])
-    n = (pt.size/10.0).ceil
-
-    a = []
-    n.times do |i|
-      a[i] = []
-      if i == n-1     # if it's the only set
-        puts "% times"
-        pt.size%10.times do |v|
-          a[i] << pt[i*10 + v] if pt[i*10 + v]
+    def set_pool(spotify_user)
+      # SETTING POOL OF TRACKS FOR NEW PLAYLIST
+      if params[:pool] == "genre"
+        genres = []
+        genre_options = [params[:genre_seed_one], params[:genre_seed_two], params[:genre_seed_three]]
+        genre_options.each do |opt|
+          unless opt == ""
+            genres << opt
+          end
         end
-      else
-        puts "10 times"
-        10.times do |v|
-          a[i] << pt[i*10 + v] if pt[i*10 + v]
+        if genres.size == 1
+          genres = genres[0].to_s
+        end
+        recs = RSpotify::Recommendations.generate(seed_genres: genres, limit: 100)
+        playlist_pool = []
+        recs.tracks.each do |track|
+          playlist_pool << track
+        end
+        puts playlist_pool
+      elsif params[:pool] == "top_tracks"
+        puts "my_top_tracks if branch"
+        playlist_pool_1 = spotify_user.saved_tracks(limit: 50, offset: 0)
+        playlist_pool_2 = spotify_user.saved_tracks(limit: 50, offset: 50)
+        playlist_pool_3 = spotify_user.saved_tracks(limit: 50, offset: 100)
+        playlist_pool_4 = spotify_user.saved_tracks(limit: 50, offset: 150)
+        
+        playlist_pool = []
+        playlist_pool += playlist_pool_1 + playlist_pool_2 + playlist_pool_3 + playlist_pool_4
+      end
+      
+      playlist_pool
+    end
+    
+    
+    def organize_tracks(playlist_time, playlist_pool)
+      # TO-DO: CREATE POOL LENGTH VARIABLE, CHECK AGAINST TRIP LENGTH; SUGGEST SAVING TRACKS OR ADDING GENRE SEEDS IF POOL LENGTH IS SHORTER
+      # RANDOMLY ADD TO PT (PLAYLIST TRACKS) ARRAY UNLESS IT WOULD EXCEED PLAYLIST_TIME
+      pt = []
+      ps = playlist_pool.size
+      puts "playlist pool size: #{ps}"
+      ptime = 0
+      until (playlist_time - ptime).abs.round(2) <= 2.50
+        if playlist_pool.size > 1
+          rn = Random.rand(playlist_pool.size-1)
+          unless ptime + playlist_pool[rn].duration_ms/60000 > playlist_time+2.50
+            pt << playlist_pool[rn]
+            playlist_pool.delete_at(rn)
+            ptime += playlist_pool[rn].duration_ms/60000
+            puts "ptime: #{ptime}"
+          end
+        else
+          flash[:notice] = "Failure building playlist pool. Try saving more tracks or selecting different genres."
+          break
+          redirect_to root_path
         end
       end
-      playlist.add_tracks!(a[i])
+    
+      puts "playlist_time: #{playlist_time}"
+      puts "ptime: #{ptime}"
+      puts pt.size
+      
+      if playlist_time - ptime > 6
+        flash[:notice] = "Your playlist pool was shorter than your trip time. Try using genre seeds or saving more Spotify tracks to your library."
+        redirect_to root_path
+      end
+      
+      pt
     end
-    puts a
 
 
+    def make_playlist(spotify_user)
+      # CREATING NEW PLAYLIST
+      playlist_name = "My Roadtrip Playlist"
+      playlist_name += " #{params[:directions][:start]} to #{params[:directions][:destination]}" if params[:directions][:start].size > 0 && params[:directions][:destination].size > 0
+      
+      puts "Creating playlist!"
+      playlist = spotify_user.create_playlist!(playlist_name)
+    end
+    
+    
+    def add_tracks(pt, playlist)
+      puts "Adding tracks!"
+      n = (pt.size/10.0).ceil
+  
+      a = []
+      n.times do |i|
+        a[i] = []
+        if i == n-1     # if it's the only set
+          puts "% times"
+          pt.size%10.times do |v|
+            a[i] << pt[i*10 + v] if pt[i*10 + v]
+          end
+        else
+          puts "10 times"
+          10.times do |v|
+            a[i] << pt[i*10 + v] if pt[i*10 + v]
+          end
+        end
+        playlist.add_tracks!(a[i])
+      end
+      puts a
+    end
+    
+    spotify_user = RSpotify::User.new(session['spotify_user'])
+    
+    playlist_time = set_time
+    playlist_pool = set_pool(spotify_user)
+    pt = organize_tracks(playlist_time, playlist_pool)
+    playlist = make_playlist(spotify_user)
+    add_tracks(pt, playlist)
+    
     redirect_to root_path
+    
   end
 
   # PATCH/PUT /playlists/1
