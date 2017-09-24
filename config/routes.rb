7293b465faeb6b86ff9a21015716585890993017
Jamison Rubino <jamison.rubino@gmail.com>
2017-09-24 22:47:58 +0000

    Changed margins, added back users controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  resources :users
-  # resources :songs
   resources :playlists, only: [:index, :show, :new, :create]
-  # resources :users
   get '/logout', to: 'users#logout'
   get '/auth/spotify/callback', to: 'users#spotify'
   post '/make_playlist', to: 'playlists#create'
