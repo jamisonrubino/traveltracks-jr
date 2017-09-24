26e0ff84b0a79cddab9ff8e41af53f131fb1c0a3
Jamison Rubino <jamison.rubino@gmail.com>
2017-09-24 23:37:09 +0000

    Finding auth token expiry time. Fixed top tracks pool size.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,12 +3,13 @@ class UsersController < ApplicationController
   
   def spotify
     session['spotify_user'] = RSpotify::User.new(request.env['omniauth.auth']).to_hash
+    session[:expires_at] = Time.current + 
     
     session['spotify_user_id'] = session['spotify_user']['id']
     
+    puts session['spotify_user']
     puts session['spotify_user_id']
     puts session['spotify_user']['email']
-    
 
     redirect_to root_path
   end
