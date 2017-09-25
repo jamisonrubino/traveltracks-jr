38c18efc77aea823fd016a589d305d2df8d5f9e6
Jamison Rubino <jamison.rubino@gmail.com>
2017-09-25 03:04:21 +0000

    Testing event listeners in welcome.html.erb instead of welcome.js

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,25 +1,27 @@
-$(document).ready(function() {
-    $("input#pool_genre").change(addHidePool);
-    $("input#pool_top_tracks").change(addHidePool);
-    $("input#pool_saved_tracks").change(addHidePool);
-    $("input#pool_artist").change(addHidePool)
-});
+// $(document).ready(function() {
+//     addEventListeners();
+// });
 
-var genreNum = "two";
+// function addEventListeners() {
+//     $("input#pool_genre").change(addHidePool);
+//     $("input#pool_top_tracks").change(addHidePool);
+//     $("input#pool_saved_tracks").change(addHidePool);
+//     $("input#pool_artist").change(addHidePool)
+// }
 
-function addHidePool() {
-    if ($("input:radio[name=pool]#pool_genre").is(":checked")) {
-        $("#artist-input").removeClass();
-        $("#artist-input").addClass("hide");
-        $("#genre-seeds").removeClass("hide");
-    } else if ($("input:radio[name=pool]#pool_top_tracks").is(":checked") || $("input:radio[name=pool]#pool_saved_tracks").is(":checked")) {
-        $("#genre-seeds").removeClass();
-        $("#artist-input").removeClass();
-        $("#genre-seeds").addClass("hide");
-        $("#artist-input").addClass("hide");
-    } else if ($("input:radio[name=pool]#pool_artist").is(":checked")) {
-        $("#artist-input").removeClass("hide");
-        $("#genre-seeds").removeClass();
-        $("#genre-seeds").addClass("hide");
-    }
-}
+// function addHidePool() {
+//     if ($("input:radio[name=pool]#pool_genre").is(":checked")) {
+//         $("#artist-input").removeClass();
+//         $("#artist-input").addClass("hide");
+//         $("#genre-seeds").removeClass();
+//     } else if ($("input:radio[name=pool]#pool_top_tracks").is(":checked") || $("input:radio[name=pool]#pool_saved_tracks").is(":checked")) {
+//         $("#genre-seeds").removeClass();
+//         $("#artist-input").removeClass();
+//         $("#genre-seeds").addClass("hide");
+//         $("#artist-input").addClass("hide");
+//     } else if ($("input:radio[name=pool]#pool_artist").is(":checked")) {
+//         $("#artist-input").removeClass();
+//         $("#genre-seeds").removeClass();
+//         $("#genre-seeds").addClass("hide");
+//     }
+// }
