02f040edc8ce4950db5fce2c46a13001d316f0b3
Jamison Rubino <jamison.rubino@gmail.com>
2017-10-04 16:54:20 +0000

    Changed precompile settings, set to production, moved playlist_form.js code to _playlist_form.html.erb

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.assets.compile = true
+# Rails.application.config.assets.compile = true
 
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
@@ -14,5 +14,5 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
-Rails.application.config.assets.precompile += ['*.js', '*.css', '*.css.erb'] 
-# Rails.application.config.assets.precompile += %w( )
+# Rails.application.config.assets.precompile += ['*.js', '*.css', '*.css.erb'] 
+Rails.application.config.assets.precompile += %w( playlist_form.js autocomplete.js )
