cd7ced45ae39d79553e4338aad953abf60040f28
Jamison Rubino <jamison.rubino@gmail.com>
2017-10-16 05:55:11 +0000

    Resized icons, precompiled assets.

--- app/assets/javascripts/components/playlists.js.jsx
+++ app/assets/javascripts/components/playlists.js.jsx
@@ -10,7 +10,7 @@ var Playlists = React.createClass({
                         <tr>
                             <td><a href={"/playlists/"+playlist.id}>{playlist.title}</a></td>
                             <td className="date"><a href={"/playlists/"+playlist.id}>{playlist.created_at.toString().substr(0,10)}</a></td>
-                            <td className="delete"><a href={"/playlists/"+playlist.id} data-method="delete" data-confirm="Are you sure?"><img src={trash} width="20px" height="20px" className="delete-trash" /></a></td>
+                            <td className="delete"><a href={"/playlists/"+playlist.id} data-method="delete" data-confirm="Are you sure?"><img src={trash} width="14px" height="16px" className="delete-trash" /></a></td>
                         </tr>
                     )}
                 </tbody>
